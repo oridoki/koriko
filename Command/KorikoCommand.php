44f32e871e694ff312885460ce41bd90a7ab0a33
Kpacha <kpacha666@gmail>
2013-03-27 20:55:40 +0100

    Ooops, sorry for that...

--- Command/KorikoCommand.php
+++ Command/KorikoCommand.php
@@ -4,7 +4,6 @@ namespace Oridoki\Koriko\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Oridoki\Koriko\Recipes\DummyRecipe;
@@ -15,25 +14,11 @@ class KorikoCommand extends Command
     {
         $this
             ->setName('recipe:dummy')
-            ->setDescription('Dummy recipe')
-            ->addArgument(
-                'user',
-                InputArgument::REQUIRED,
-                'The user for the ssh2 connection'
-            )
-            ->addArgument(
-                'password',
-                InputArgument::REQUIRED,
-                'The password for the connection'
-            );
+            ->setDescription('Dummy recipe');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-//        print_r($input);
-//        print_r($output);
-        $name = $input->getArgument('user');
-        $name = $input->getArgument('password');
         $recipe = new DummyRecipe;
         $recipe->cook();
     }

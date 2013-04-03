a63c6603f13c8e3322ae6b05e9762fabe97261a5
Kpacha <kpacha666@gmail>
2013-04-03 20:22:28 +0200

    fixing the length of lines

--- App/Application.php
+++ App/Application.php
@@ -5,15 +5,15 @@ namespace Oridoki\Koriko\App;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Finder\Finder;
 
-class Application extends BaseApplication
-{
+class Application extends BaseApplication {
 
     const NAME = 'Koriko Deploy System';
     const VERSION = '0.1';
     const DEFAULT_NAMESPACE = '\\Oridoki\\Koriko\\Command\\';
     const DEFAULT_COMMAND_FOLDER = '/../Command';
 
-    public function __construct($commandFolder = self::DEFAULT_COMMAND_FOLDER, $namespace = self::DEFAULT_NAMESPACE)
+    public function __construct($commandFolder = self::DEFAULT_COMMAND_FOLDER,
+            $namespace = self::DEFAULT_NAMESPACE)
     {
         parent::__construct(static::NAME, static::VERSION);
 
@@ -26,7 +26,8 @@ class Application extends BaseApplication
         $finder->files()->name('*Command.php')->in(__DIR__ . $commandFolder);
 
         foreach ($finder as $file) {
-            $command = $namespace . str_replace('.php', '', $file->getRelativePathname());
+            $command = $namespace . str_replace('.php', '',
+                    $file->getRelativePathname());
             $this->add(new $command);
         }
     }
